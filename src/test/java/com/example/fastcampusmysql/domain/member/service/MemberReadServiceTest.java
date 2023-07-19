9060adb4ebf52af333113cd765ecece35205b689
kym9129 <kym9129@naver.com>
2023-07-19 22:56:46 +0900

    test: MemberReadService 단위테스트 추가

---
+++ src/test/java/com/example/fastcampusmysql/domain/member/service/MemberReadServiceTest.java
@@ -0,0 +1,98 @@
+package com.example.fastcampusmysql.domain.member.service;
+
+import com.example.fastcampusmysql.domain.member.dto.MemberDto;
+import com.example.fastcampusmysql.domain.member.dto.MemberNicknameHistoryDto;
+import com.example.fastcampusmysql.domain.member.entity.Member;
+import com.example.fastcampusmysql.domain.member.entity.MemberNicknameHistory;
+import com.example.fastcampusmysql.domain.member.repository.MemberNicknameHistoryRepository;
+import com.example.fastcampusmysql.domain.member.repository.MemberRepository;
+import com.example.fastcampusmysql.utill.MemberFixtureFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class MemberReadServiceTest {
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private MemberNicknameHistoryRepository memberNicknameHistoryRepository;
+
+    @InjectMocks
+    private MemberReadService memberReadService;
+
+    private List<Member> mockMemberList;
+
+    @BeforeEach
+    void setUp() {
+        mockMemberList = new ArrayList<>();
+        for(int i=0; i<10; i++){
+            mockMemberList.add(MemberFixtureFactory.create());
+        }
+    }
+
+    @DisplayName("회원 id로 회원 정보를 조회하여 dto객체로 리턴한다")
+    @Test
+    void testGetMember() {
+        // given
+        Member mockMember = mockMemberList.get(0);
+        given(memberRepository.findById(anyLong())).willReturn(Optional.of(mockMember));
+        // when
+        MemberDto resultMember = memberReadService.getMember(mockMember.getId());
+        // then
+        assertThat(resultMember.nickname(), is(mockMember.getNickname()));
+    }
+
+    @DisplayName("N개의 회원 id로 한번에 회원정보 조회. 리스트를 리턴")
+    @Test
+    void testGetMembers() {
+        // given
+        List<Long> ids = mockMemberList.stream()
+                .map(Member::getId)
+                .toList();
+
+        given(memberRepository.findAllByIdIn(any())).willReturn(mockMemberList);
+
+        // when
+        List<MemberDto> members = memberReadService.getMembers(ids);
+
+        // then
+        Assertions.assertThat(members).extracting("id").containsAll(ids);
+    }
+
+    @DisplayName("회원ID로 닉네임 변경 이력을 조회한다.")
+    @Test
+    void testGetNicknameHistory() {
+        // given
+        Member mockMember = mockMemberList.get(0);
+        int historySize = 10;
+        List<MemberNicknameHistory> givenHistoryList = MemberFixtureFactory.getMemberNicknameHistoryList(mockMember.getId(), historySize);
+
+        given(memberNicknameHistoryRepository.findAllByMemberId(anyLong())).willReturn(givenHistoryList);
+
+        // when
+        List<MemberNicknameHistoryDto> nicknameHistoryList = memberReadService.getNicknameHistory(mockMember.getId());
+
+        // then
+        for (int i=0; i<historySize; i++) {
+            assertThat(nicknameHistoryList.get(i).nickname(), is(givenHistoryList.get(i).getNickname()));
+        }
+    }
+}
--- src/test/java/com/example/fastcampusmysql/utill/MemberFixtureFactory.java
+++ src/test/java/com/example/fastcampusmysql/utill/MemberFixtureFactory.java
@@ -1,8 +1,16 @@
 package com.example.fastcampusmysql.utill;
 
 import com.example.fastcampusmysql.domain.member.entity.Member;
+import com.example.fastcampusmysql.domain.member.entity.MemberNicknameHistory;
 import org.jeasy.random.EasyRandom;
 import org.jeasy.random.EasyRandomParameters;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.jeasy.random.FieldPredicates.*;
 
 public class MemberFixtureFactory {
     // 테스트 코드 작성 시 허들 중 하나 : 테스트 객체를 어떻게 생성할 것인가
@@ -17,5 +25,28 @@ public class MemberFixtureFactory {
     public static Member create(Long seed){ // todo: 시드도 factory 내에서 랜덤하게 생성
         EasyRandomParameters param = new EasyRandomParameters().seed(seed);
         return new EasyRandom(param).nextObject(Member.class);
+    }
+
+    public static List<MemberNicknameHistory> getMemberNicknameHistoryList(Long memberId, int size) {
+        // memberId는 파라미터 받은거 고정으로 넣고 id, nickname, createdAt을 랜덤으로 넣기
+        Predicate<Field> idPredicate = named("id")
+                .and(ofType(Long.class))
+                .and(inClass(MemberNicknameHistory.class));
+
+        Predicate<Field> memberIdPredicate = named("memberId")
+                .and(ofType(Long.class))
+                .and(inClass(MemberNicknameHistory.class));
+
+        EasyRandomParameters param = new EasyRandomParameters()
+                .excludeField(idPredicate) // id null
+                .randomize(memberIdPredicate, () -> memberId); // memberId 주입받은걸로 변환
+
+        EasyRandom easyRandom = new EasyRandom(param);
+        List<MemberNicknameHistory> memberNicknameHistoryList = new ArrayList<>();
+        for (int i=0; i<size; i++){
+            memberNicknameHistoryList.add(easyRandom.nextObject(MemberNicknameHistory.class));
+        }
+
+        return memberNicknameHistoryList;
     }
 }
