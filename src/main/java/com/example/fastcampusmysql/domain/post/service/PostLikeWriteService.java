8aa79ac19014e747069e169876df0cff2e6ebed5
kym9129 <kym9129@naver.com>
2023-07-26 02:30:34 +0900

    refactor: 좋아요 기능에 Redis 적용

--- src/main/java/com/example/fastcampusmysql/domain/post/service/PostLikeWriteService.java
+++ src/main/java/com/example/fastcampusmysql/domain/post/service/PostLikeWriteService.java
@@ -5,20 +5,29 @@ import com.example.fastcampusmysql.domain.post.entity.Post;
 import com.example.fastcampusmysql.domain.post.entity.PostLike;
 import com.example.fastcampusmysql.domain.post.repository.PostLikeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class PostLikeWriteService {
     private final PostLikeRepository postLikeRepository;
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Value("${spring.redis.key.post_like_prefix}")
+    private String POST_LIKE_KEY_PREFIX;
 
     public Long create(Post post, MemberDto memberDto) {
+        SetOperations<String, String> setOps = redisTemplate.opsForSet();
+        setOps.add(POST_LIKE_KEY_PREFIX+post.getId(), String.valueOf(memberDto.id()));
+
         PostLike postLike = PostLike
                 .builder()
                 .postId(post.getId())
                 .memberId(memberDto.id())
                 .build();
-
         return postLikeRepository.save(postLike).getPostId();
     }
 
